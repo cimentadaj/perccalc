0b977d4542d0906a0efa462b9aeb1cd41c258204
cimentadaj <cimentadaj@gmail.com>
2017-07-31 12:38:09 +0200

    Fixed ordered factor check

--- R/perc_calculator.R
+++ R/perc_calculator.R
@@ -93,7 +93,7 @@ category_summary <- function(data_model,
                              weights,
                              prefix = "cat_") {
 
-  stopifnot(class(data_model[[categorical_var]]) %in% c("ordered", "factor"))
+  stopifnot(all(class(data_model[[categorical_var]]) %in% c("ordered", "factor")))
   data_model <- data_model[stats::complete.cases(data_model), ]
 
   category_columns <- levels(data_model[[categorical_var]])
