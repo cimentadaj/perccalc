cf27db0f5d83f0b6c7b1a768424d8e9d772c41d7
Jorge Cimentada <cimentadaj@gmail.com>
2019-09-23 16:55:05 +0200

    Add citation

---
+++ R/utils.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+   packageStartupMessage("\nPlease cite as: \n")
+   packageStartupMessage("Cimentada, Jorge (2019). Estimate Percentiles from an Ordered Categorical Variable R package version 1.0.3.")
+}
