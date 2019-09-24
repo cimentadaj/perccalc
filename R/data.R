7fb88b658a47a76c90f4920780366d6b1a0df67f
Jorge Cimentada <cimentadaj@gmail.com>
2019-09-24 11:40:28 +0200

    Adds PISA datasets

---
+++ R/data.R
@@ -0,0 +1,41 @@
+#' Mathematics test scores of Spain, Germany and Estonia in the PISA 2012 test
+#'
+#' A dataset containing the test scores and other household information of
+#' students from Spain, Germany and Estonia from the PISA 2012 test.
+#'
+#' @format A data frame with 35093 rows and 10 variables:
+#' \describe{
+#'   \item{year}{Year of the survey}
+#'   \item{CNT}{Long country names}
+#'   \item{STIDSTD}{Unique student id}
+#'   \item{PV1MATH}{The first mathematics test scores for each student}
+#'   \item{PV2MATH}{The second mathematics test scores for each student}
+#'   \item{PV3MATH}{The third mathematics test scores for each student} 
+#'   \item{PV4MATH}{The fourth mathematics test scores for each student} 
+#'   \item{PV5MATH}{The fifth mathematics test scores for each student} 
+#'   \item{father_edu}{The father's highest achieved degree in the ISCED scale}
+#'   \item{household_income}{The household's total income in categories}
+#' }
+#' @source A subset extracted from the \code{PISA2012lite} R package, https://github.com/pbiecek/PISA2012lite 
+"pisa_2012"
+
+#' Mathematics test scores of Spain, Germany and Estonia in the PISA 2006 test
+#'
+#' A dataset containing the test scores and other household information of
+#' students from Spain, Germany and Estonia from the PISA 2006 test.
+#'
+#' @format A data frame with 25884 rows and 10 variables:
+#' \describe{
+#'   \item{year}{Year of the survey}
+#'   \item{CNT}{Long country names}
+#'   \item{STIDSTD}{Unique student id}
+#'   \item{PV1MATH}{The first mathematics test scores for each student}
+#'   \item{PV2MATH}{The second mathematics test scores for each student}
+#'   \item{PV3MATH}{The third mathematics test scores for each student} 
+#'   \item{PV4MATH}{The fourth mathematics test scores for each student} 
+#'   \item{PV5MATH}{The fifth mathematics test scores for each student} 
+#'   \item{father_edu}{The father's highest achieved degree in the ISCED scale}
+#'   \item{household_income}{The household's total income in categories}
+#' }
+#' @source A subset extracted from the \code{PISA2006lite} R package, https://github.com/pbiecek/PISA2012lite 
+"pisa_2006"
