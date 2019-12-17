c7846f50c05e9e4f9c44ca9596238accf67231b0
Jorge Cimentada <cimentadaj@gmail.com>
2019-12-17 16:27:22 +0100

    Updates citation on startup

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,8 @@
-# perccalc 1.0.4.9999
+# perccalc 1.0.5.9999
+
+# perccalc 1.0.4
+
+* JOSS [accepted](https://joss.theoj.org/papers/10.21105/joss.01796) the paper!
 
 ### Minor changes
 * Both exported functions now accept a data frame with any number of variables and only the categorial, continuos and weights variables are used.
--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
    packageStartupMessage("\nPlease cite as: \n")
-   packageStartupMessage("Cimentada, Jorge (2019). Estimate Percentiles from an Ordered Categorical Variable R package version 1.0.3.")
+   packageStartupMessage("Cimentada, (2019). perccalc: An R package for estimating percentiles from categorical variables. Journal of Open Source Software, 4(44), 1796, https://doi.org/10.21105/joss.01796")
 }
